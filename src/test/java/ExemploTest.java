0c47366976a951776d3c598f73eaf344d98c4a2d
Joao Arthur Brunet Monteiro <joaoarthurbm@gmail.com>
2024-01-25 18:42:22 -0300

    mvn

---
+++ src/test/java/ExemploTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class ExemploTest {
+    
+    @Test
+    public void testMessage() {
+        // TODO
+    }
+}
